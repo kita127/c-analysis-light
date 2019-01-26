a30fc495dc382d3e3f30f50118d3aaffefba2078
kita127 <kitada.kosuke@gmail.com>
2019-01-26 23:33:22 +0900

    データ型定義

--- src/Language/C/Analysis/Light.hs
+++ src/Language/C/Analysis/Light.hs
@@ -5,6 +5,22 @@ module Language.C.Analysis.Light
 
 import qualified Data.Text as T
 
+data C = Prepro T.Text C
+       | Csrc Cstate C
+    deriving (Eq, Show)
+
+
+data Cstate = Var
+              { typ     :: T.Text
+              , name    :: T.Text
+              , initVal :: Maybe T.Text
+              }
+            | Func
+              { name    :: T.Text
+              }
+            deriving (Eq, Show)
+
+
 
 analyze :: T.Text -> T.Text
 analyze = id

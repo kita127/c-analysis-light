e3951869a2c467567272d20068651e9aad70ff39
kita127 <kitada.kosuke@gmail.com>
2019-02-09 22:52:59 +0900

    sample_00.c 更新

--- examples/sample_00.c
+++ examples/sample_00.c
@@ -1,9 +1,11 @@
 
-/* definition global variable */
-int global_variable = 99;
 
 #if HOGE_SW == 1
 
 char condition_variable;
+static xxx_condVar = 100;
 
 #endif    /* HOGE_SW */
+
+/* definition global variable */
+int global_variable = 99;

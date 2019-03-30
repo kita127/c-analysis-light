7422ceb6470fbccc3b5498311722367005ca13b2
kita127 <kitada.kosuke@gmail.com>
2019-03-30 12:04:42 +0900

    main_00.c 更新

--- examples/main_00.c
+++ examples/main_00.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+#include    "../../external_inc.h"
 
-#define CONDITION       HOGE
+#define HOGE    (1)
+#define FUGA    (2)
+#define VARI    HOGE
 
+
+#if VARI == HOGE
+char hoge_globvar = 100;
+  #if VARI_2 == HOGE
+static int hoge_globvar_2 = xxx;
+  #endif
+#endif    /* VARI */
+
+int arr_var[10];
 
 int main( void )
 {
     int local_var;
+    local_var = 0;
+    local_var++;
 
-    local_var = 1 + 2;
-
-    printf("local_var ...%d\n", local_var);
+    printf("local_var...%d\n", local_var);
 
     return (0);
 }
